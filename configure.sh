520b8643c282e2df63cdd5f88027b64645d98a54
小幻 <68892893+xhuanya@users.noreply.github.com>
2022-03-28 16:58:57 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
  mkdir ~/.screen && chmod 700 ~/.screen
  export SCREENDIR=$HOME/.screen
  sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf
- bash <(curl -fsSL git.io/warp.sh) s5
+
 #下载xray
 xray(){
 wget -O /opt/xray.zip $xray
